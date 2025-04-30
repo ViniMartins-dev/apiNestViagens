b6f5a29c876acc554c96903fd1170c6faa0fe678
vinicius martins <vinicius_apple22@outlook.com>
2025-04-29 21:26:25 -0300

    a

--- src/quarto/quarto.service.ts
+++ src/quarto/quarto.service.ts
@@ -6,13 +6,39 @@ import { Quarto } from './quarto.entity';
 @Injectable()
 export class QuartoService {
 
-    constructor(
-      @InjectRepository(Quarto)
-      private quartoRepository: Repository<Quarto>,
-    ) { }
-  
-    findAll(): Promise<Quarto[]> {
-      return this.quartoRepository.find();
-    }
-  
+  constructor(
+    @InjectRepository(Quarto)
+    private quartoRepository: Repository<Quarto>,
+  ) { }
+
+  async findAll(): Promise<Quarto[]> {
+    return this.quartoRepository.find();
   }
+
+  async create(data: Partial<Quarto>): Promise<Quarto> {
+    const quarto = this.quartoRepository.create(data);
+    return this.quartoRepository.save(quarto);
+  }
+
+  async show(id: number): Promise<Quarto> {
+    const quarto = await this.quartoRepository.findOne({ where: { id } });
+
+    if(!quarto) {
+      throw new NotFoundException("Quarto não encontrado")
+    }
+
+    return quarto;
+  }
+
+  async patch(id: number, status: Partial<Quarto>): Promise<Quarto> {
+    const quarto = await this.quartoRepository.findOne({ where: {id} });
+
+    if (!quarto) {
+      throw new NotFoundException("Quarto não encontrado")
+    }
+
+    const updated = Object.assign(quarto, status);
+    return await this.quartoRepository.save(updated);
+  }
+
+}
