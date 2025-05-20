e5e94bcb93fa6c1a7bd4c31513eaebf1eea14e92
vinicius <vinicius_apple22@outlook.com>
2025-05-20 15:04:43 -0300

    feat: metodos na controller

--- src/quarto/quarto.controller.ts
+++ src/quarto/quarto.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Param,
+    Body,
+    ParseIntPipe,
+} from '@nestjs/common';
+import { QuartoService } from './quarto.service';
+import { Quarto } from './quarto.entity';
 
 @Controller('quarto')
-export class QuartoController {}
+export class QuartoController {
+    constructor(private readonly quartoService: QuartoService) { }
+
+    // GET /quarto
+    @Get()
+    findAll(): Promise<Quarto[]> {
+        return this.quartoService.findAll();
+    }
+
+    // POST /quarto
+    @Post()
+    create(@Body() data: Partial<Quarto>): Promise<Quarto> {
+        return this.quartoService.create(data);
+    }
+
+    // GET /quarto/:id
+    @Get(':id')
+    show(@Param('id', ParseIntPipe) id: number): Promise<Quarto> {
+        return this.quartoService.show(id);
+    }
+
+    // PATCH /quarto/:id
+    @Patch(':id')
+    patch(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() status: Partial<Quarto>,
+    ): Promise<Quarto> {
+        return this.quartoService.patch(id, status);
+    }
+}
--- src/quarto/quarto.module.ts
+++ src/quarto/quarto.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuartoService } from './quarto.service';
 import { QuartoController } from './quarto.controller';
+import { Quarto } from './quarto.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Quarto])],
   providers: [QuartoService],
-  controllers: [QuartoController]
+  controllers: [QuartoController],
 })
 export class QuartoModule {}
